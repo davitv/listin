ed6d51297e3451cd39626fc90e98da9a2ab69eee
Davit Vardanyan <davoimail@gmail.com>
2016-10-03 11:47:35 +0300

    Fix: remove local.py.example, set correct static root

--- listin/settings/base.py
+++ listin/settings/base.py
@@ -161,7 +161,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 # Additional locations of static files
 STATICFILES_DIRS = [
